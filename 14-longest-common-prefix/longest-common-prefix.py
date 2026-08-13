17f4b3bf0674c3fb1b8c74b0a7818d0e0c90251f
Vikas HV <vickyh18a@gmail.com>
2026-08-13 15:32:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (69.46%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        strs.sort()
+
+        first =strs[0]
+        last = strs[-1]
+
+        ans = ""
+         
+        for i in range(min(len(first),len(last))):
+            if first[i] == last[i]:
+                ans = ans+first[i]
+            else:
+                break
+        return ans
+
+            
+
+        
