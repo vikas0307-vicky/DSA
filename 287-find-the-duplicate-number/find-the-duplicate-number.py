6d9f10b7933f934824b28caf1e0fe0640d884584
Vikas HV <vickyh18a@gmail.com>
2026-08-24 21:59:19 +0530

    Time: 27 ms (88.00%) | Memory: 27.2 MB (15.54%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,5 +1,9 @@
 class Solution(object):
     def findDuplicate(self, nums):
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]==nums[j]:
+        #             return nums[i]
         s1= set()
         for i in nums:
             if i in s1:
