eb30603575080693cba3d84c8cdfad7776ac4b6f
Vikas HV <vickyh18a@gmail.com>
2026-08-25 18:07:48 +0530

    Time: 31 ms (90.90%) | Memory: 13.9 MB (53.37%) - LeetSync

---
+++ 561-array-partition/array-partition.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        nums.sort()
+        total = 0
+
+        for i in  range(0,len(nums),2):
+            total = total +nums[i]
+
+
+        return total
+        
