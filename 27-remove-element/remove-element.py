0f25658a64e6dec908ab23b812c1c22b73d6067d
Vikas HV <vickyh18a@gmail.com>
2026-08-31 21:52:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (17.20%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        i = 0
+        for x in nums:
+            if x!= val:
+                nums[i]=x
+                i=i+1
+        return i
+
+        
