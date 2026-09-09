9bb08d6d070478c57435d77c630f3b4820954ef2
Vikas HV <vickyh18a@gmail.com>
2026-09-09 19:51:37 +0530

    Time: 16 ms (59.79%) | Memory: 12.6 MB (21.04%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        result = ""
+        for i in range(max(len(word1),len(word2))):
+
+            if i <len(word1):
+                result = result + word1[i]
+
+            if i <len(word2):
+                result = result + word2[i]
+
+        return("".join(result))
