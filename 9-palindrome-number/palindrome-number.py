0c8355eebf336dc04e6f57d1c955b8e578e7e62e
Vikas HV <vickyh18a@gmail.com>
2026-09-11 19:21:09 +0530

    Time: 8 ms (72.29%) | Memory: 12 MB (99.96%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -9,8 +9,12 @@
 
 class Solution(object):
     def isPalindrome(self, x):
+        # rev = str(x)[::-1]
+        # if str(x) == rev:
+        #     return True
+        # else:
+        #     return False
         rev = str(x)[::-1]
-
         if str(x) == rev:
             return True
         else:
