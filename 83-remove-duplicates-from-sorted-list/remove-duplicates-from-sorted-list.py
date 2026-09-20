aaef820235de1d4ddde8b30f8441c4bc149c9330
Vikas HV <vickyh18a@gmail.com>
2026-09-20 18:56:36 +0530

    Time: 1 ms (55.47%) | Memory: 12.3 MB (61.45%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -13,11 +13,12 @@ class Solution(object):
         #         curr = curr.next
         
         # return head
-        current=head
-        while current:
-            while current.next and current.next.val==current.val:
-                current.next=current.next.next
-            current=current.next
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next
+            cur  = cur.next
+
         return head
 
     
