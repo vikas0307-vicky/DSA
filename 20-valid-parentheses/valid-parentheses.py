b4b9c63528d26ecf63dfc74e16c5a3f1e42e7059
Vikas HV <vickyh18a@gmail.com>
2026-09-21 22:48:11 +0530

    Time: 2 ms (81.59%) | Memory: 12.4 MB (73.45%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,6 +1,29 @@
 class Solution(object):
     def isValid(self, s):
         stack =[]
+        # n = len(s)
+        # if n%2==0:
+        #     return False
+
+        # st = []
+
+        # for i in list(s):
+        #     if i == "(" or i =="{" or i =="[":
+        #         st.append(i)
+        #     else:
+        #         if len(st) == 0:
+        #             return False
+        #         top = st.pop()
+        #         if i == ")" and top != "(":
+        #             return False
+        #         elif i == "}" and top != "{":
+        #             return False
+        #         elif i == "]" and top != "[":
+        #             return False
+
+        # return len(st) == 0
+ 
+
         mp ={
             ')':'(',
             ']':'[',
